9f89cd7251edf0012d63b6014e2e4a402371d7bc
Christopher Sardegna <github@reagentx.net>
2018-05-05 21:45:22 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='scraper-platform',
+    version='1.0',
+    description='A versatile Python 3 scraper platform.',
+    author='Christopher Sardegna',
+    author_email='github@reagentx.net',
+    scripts=['']
+)
